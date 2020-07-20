613e16c251ffb9a0a8e42c858f7e9764776089d9
centerRyo <abc.atrn@gmail.com>
2020-07-20 13:01:59 +0900

    タスクの追加、削除を行う

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,53 @@ import ReactDOM from 'react-dom';
 import TodoCreator from './components/TodoCreator';
 import Search from './components/Search';
 import TodoList from './components/TodoList';
+import _ from 'lodash';
 
 class TodoApp extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [
+        {
+          id: this.createHashId(),
+          text: 'sample todo01'
+        },
+        {
+          id: this.createHashId(),
+          text: 'sample todo02'
+        }
+      ]
+    };
+
+    this.callBackAddTask = this.callBackAddTask.bind(this);
+    this.callBackRemoveTask = this.callBackRemoveTask.bind(this);
+  }
+
+  createHashId() {
+    return Math.random().toString(36).slice(-16);
+  }
+
+  callBackAddTask(val) {
+    let nextData = this.state.data;
+    nextData.push({id: this.createHashId(), text: val});
+    this.setState({
+      data: nextData
+    });
+  }
+
+  callBackRemoveTask(id) {
+    let data = _.reject(this.state.data, {'id': id});
+    this.setState({
+      data: data
+    });
   }
 
   render() {
     return (
       <div>
-        <TodoList />
+        <TodoCreator callBackAddTask={this.callBackAddTask} />
+
+        <TodoList data={this.state.data} callBackRemoveTask={this.callBackRemoveTask} />
       </div>
     );
   }
